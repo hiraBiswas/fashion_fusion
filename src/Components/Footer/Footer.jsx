1c969a76604d329a2e798708b618a6c01bbcb557
hiraBiswas <hbhirabiswas@gmail.com>
2024-02-21 12:04:28 +0600

    footer added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-16 p-10 bg-base-200 text-base-content">
+  <aside>
+  <img src="./logo.png" className='h-24 w-24' alt="" />
+    <p>FashionFusion Ltd.<br/>Making tradition the trend since 2017</p>
+  </aside> 
+  <nav>
+    <h6 className="footer-title">Services</h6> 
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav> 
+  <nav>
+    <h6 className="footer-title">Company</h6> 
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav> 
+  <nav>
+    <h6 className="footer-title">Legal</h6> 
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+    );
+};
+
+export default Footer;
