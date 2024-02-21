1b40feebd4ba0640d30358ca7abe300298a1a9d8
hiraBiswas <hbhirabiswas@gmail.com>
2024-02-21 09:02:35 +0600

    inital set up done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcwx5iwTMfXj7ouNBaUS4Yt8YHTb7Ebl8",
+  authDomain: "fashionfusion-dbf8a.firebaseapp.com",
+  projectId: "fashionfusion-dbf8a",
+  storageBucket: "fashionfusion-dbf8a.appspot.com",
+  messagingSenderId: "499691106611",
+  appId: "1:499691106611:web:a6fd08f257935e3927ce46"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/main.jsx
@@ -0,0 +1,71 @@
+import React, { Children } from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+import Root from './Pages/Root/Root';
+import ErrorPage from './Pages/ErrorPage/ErrorPage';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+import AuthProvider from './Providers/AuthProvider';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage> ,
+    children:[
+      {
+        path:"/",
+        element: <Home></Home>,
+        // loader:()=>fetch('/data.json')
+
+      },
+
+      // {
+      //   path:"/events/:id",
+      //   element:<PrivateRoute><EventDetails></EventDetails></PrivateRoute>,
+      //   loader:()=>fetch('/data.json')
+      // },
+     
+      // {
+      //   path:"/decoration",
+      //   element: <PrivateRoute><ThemeDecoration></ThemeDecoration></PrivateRoute>
+      // },
+
+      {
+        path:"/login",
+        element:<Login></Login>
+      },
+
+      
+      // {
+      //   path:"/gallery",
+      //   element: <PrivateRoute><Gallery></Gallery></PrivateRoute>
+      // },
+
+      {
+        path:"/register",
+        element: <Register></Register>
+      },
+      
+    ]
+  },
+
+ 
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+    <RouterProvider router={router} />
+    </AuthProvider>
+   
+  </React.StrictMode>,
+)
